bec96c3f9b8f6e014f07ec83d48cfca56fdfce93
rubydusa <rubydusa@protonmail.com>
2024-02-14 22:50:33 +0200

    feat: return footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,7 @@ import { Box, Typography } from '@mui/material';
 const Footer = () => {
   return (
     <Box mt={5} py={3} textAlign="center">
-      {/* Body contents start to overlap, work on this later */}
-      {/* <Typography variant="body1">© 2024 Egotism</Typography> */}
+      <Typography variant="body1">© 2024 Egotism</Typography>
     </Box>
   );
 };
